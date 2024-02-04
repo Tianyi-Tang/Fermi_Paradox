a8a2deb724af470d3b8638886c6c54a61ad693e6
Andy Tang <tta89@sfu.ca>
2024-02-04 06:01:39 -0800

    remove the all content on Start and Update and most of attribute in Colony class and it will be treat as record resource civilization have in this planetarySystem

--- Assets/Script/Stars/Colony/Colony.cs
+++ Assets/Script/Stars/Colony/Colony.cs
@@ -4,62 +4,19 @@ using UnityEngine;
 
 public class Colony : MonoBehaviour
 {
-    private Planet currentPlanet;
-    private PlanetarySystem currentPlanetarySystem;
-    [SerializeField]private PlanetBuildingSO building;
-
-    private int basic_miningSpeed;
-    private int addtional_miningSpeed;
-
-    private bool hasResource = true;
-    private float passTime = 0;
 
 
     private int resource;
 
     void Start()
     {
-        currentPlanet = this.GetComponent<Planet>();
-        currentPlanetarySystem = GetComponentInParent<PlanetarySystem>();
+      
     }
 
     // Update is called once per frame
     void Update()
     {
-        passTime += Time.deltaTime;
-        if (passTime >= 1)
-        {
-            if (building != null && hasResource)
-            {
-                resource = currentPlanet.reduceResource(basic_miningSpeed + addtional_miningSpeed);
-                if (resource != -1)
-                {
-                    currentPlanetarySystem.addResource(resource);
-                    hasResource = false;
-
-                }
-                else
-                {
-                    currentPlanetarySystem.addResource(basic_miningSpeed + addtional_miningSpeed);
-                }
-
-                
-            }
-            passTime = 0;
-        }
-        
 
     }
 
-
-    public void setBuilding(PlanetBuildingSO building)
-    {
-        this.building = building;
-        addAddtional_miningSpeed(building.getMiningSpeed());
-    }
-
-    private void addAddtional_miningSpeed(int miningSpeed)
-    {
-        addtional_miningSpeed = miningSpeed;
-    }
 }
