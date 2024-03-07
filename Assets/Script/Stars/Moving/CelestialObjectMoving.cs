638dec4e1450b7e52df8c87efc8e829cd4838639
Andy Tang <tta89@sfu.ca>
2024-03-06 22:24:38 -0800

    create a new abstracted classcalled CelestialObjectMoving and function that need to implement

---
+++ Assets/Script/Stars/Moving/CelestialObjectMoving.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class CelestialObjectMoving
+{
+    protected float revolutionSpeed;
+
+    public abstract void celetialMoving();
+    protected abstract void rotation();
+
+}
