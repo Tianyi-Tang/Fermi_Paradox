137ee413f0104d14b6e46862a252533d44225e89
Andy Tang <tta89@sfu.ca>
2024-03-07 00:58:06 -0800

    Change the createOrbit function in PlanetMoving class and make the Planet class store the orbit

--- Assets/Script/Stars/Moving/PlanetMoving.cs
+++ Assets/Script/Stars/Moving/PlanetMoving.cs
@@ -7,7 +7,6 @@ public class PlanetMoving : CelestialObjectMoving
     [SerializeField] private float revolutionSpeed;
     [SerializeField] private float revolutionRadius;
     private Vector3 fixedStarPosition;
-    private LineRenderer orbit;
 
     public void passpassParameter(float rotationSpeed,float revolutionSpeed,float revolutionRadius,Transform fixedStar)
     {
@@ -16,14 +15,13 @@ public class PlanetMoving : CelestialObjectMoving
         this.revolutionRadius = revolutionRadius;
         this.fixedStarPosition = fixedStar.transform.position;
         getInfor = true;
-        createOrbit(fixedStar);
     }
 
-    private void createOrbit(Transform fixedStar) {
+    public LineRenderer createOrbit(Transform fixedStar) {
         GameObject gol = new GameObject { name = "Circle" };
         gol.transform.position = fixedStarPosition;
         gol.DrawCircle(revolutionRadius, 0.2f,fixedStar);
-        orbit = gol.GetComponent<LineRenderer>();
+        return gol.GetComponent<LineRenderer>();
     }
 
     private void revolution() {
