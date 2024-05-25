0a37b54e2faf9d61ba1ee18d6bd674f6b8c0f15d
Andy Tang <tta89@sfu.ca>
2024-05-24 17:20:05 -0700

    create a PlanetInfor class to treat the long parameters list in PlanetFactory

---
+++ Assets/Script/Stars/PlanetInfor.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlanetInfor
+{
+    public PlanetTypeSO type;
+    public Vector3 fixStarPosition;
+    public float distance;
+    public float rotationSpeed;
+    public float revolutionSpeed;
+}
