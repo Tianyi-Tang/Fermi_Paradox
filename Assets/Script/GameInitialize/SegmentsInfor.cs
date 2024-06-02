f6347183264908342a6613c8336ceec78dbdd130
Andy Tang <tta89@sfu.ca>
2024-06-02 10:39:29 -0700

    create a struct SegmentsInfor to store information

---
+++ Assets/Script/GameInitialize/SegmentsInfor.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public struct SegmentsInfor 
+{
+    public int largeSegmentaeNum;
+    public int middleContinedLarge;
+    public int smallContinedmiddlle;
+}
